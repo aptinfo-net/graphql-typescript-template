1db8bc33c94018823c263069fa647365fa7aa69a
Ruslan <free.changer@gmail.com>
2023-10-01 19:01:36 +0300

    Added tsx

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,9 @@
 import { startStandaloneServer } from '@apollo/server/standalone';
 
-import { server } from './app.ts';
+import { server } from './server';
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
 const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
+  listen: { port: 4000 },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
